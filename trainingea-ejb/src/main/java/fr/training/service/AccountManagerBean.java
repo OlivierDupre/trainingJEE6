183b5e836501f1524016eaec958421ee1dacfcd6
OlivierDupre <dupreolivier@gmail.com>
2013-04-17 14:40:46 +0200

    Working solution with bidirectionnal relationships.

--- trainingea-ejb/src/main/java/fr/training/service/AccountManagerBean.java
+++ trainingea-ejb/src/main/java/fr/training/service/AccountManagerBean.java
@@ -12,6 +12,7 @@ import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
@@ -23,53 +24,55 @@ import javax.persistence.TypedQuery;
 @Local(AccountManagerLocal.class)
 @TransactionManagement(TransactionManagementType.CONTAINER)
 public class AccountManagerBean implements AccountManagerLocal {
-
+    
     @PersistenceContext
     EntityManager entityManager;
-
+    
     @Override
     public Customer createCustomer(String firstName, String lastName, String address, int age) {
         Customer customer = new Customer(firstName, lastName, address, age);
-
+        
         entityManager.persist(customer);
-
+        
         return customer;
     }
-
+    
     @Override
     public Account createAccount(Customer owner) {
         Account account = new Account(owner);
-
+        
         entityManager.persist(account);
-
+        
         return account;
     }
-
+    
     @Override
     public Customer findCustomer(String firstName, String lastName) {
         return entityManager.find(Customer.class, new CustomerPK(firstName, lastName));
     }
-
+    
     @Override
     public List<Account> findAccountsForCustomer(String firstName, String lastName) {
         TypedQuery<Account> accounts = entityManager.createNamedQuery("Customer.findAllAccounts", Account.class);
         // entityManager.getReference permet de récupérer la clef primaire de l'objet sans charger aucun des objets liés (LAZY ou même EAGER)
         accounts.setParameter("customer", entityManager.getReference(Customer.class, new CustomerPK(firstName, lastName)));
-
+        
         return accounts.getResultList();
     }
-
+    
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRED) // Valeur par défaut
     public void addPrimaryAccount(CustomerPK customerPK, float deposite) {
-        Customer customer = entityManager.find(Customer.class, customerPK);
-
+        Customer customer = entityManager.find(Customer.class, customerPK, LockModeType.PESSIMISTIC_WRITE);
+        
         Account account = new Account();
         account.setAmount(deposite);
         // Pour générer correctement la requête UPDATE sur le table
         account.setOwner(customer);
 
-        // Pour conserverle cache de niveau 2 cohérent
+        // Pour conserver le cache de niveau 2 cohérent
         customer.addPrimaryAccount(account);
+        
+        entityManager.persist(account);
     }
 }
